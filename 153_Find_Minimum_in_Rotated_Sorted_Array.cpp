3deaf0686ed4dbd76e6c7e4e0686d7c1010e6e90
Omar Abouelleil <oabouelleil@live.com>
2022-09-12 17:38:00 +0100

    Add 153

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define RUN_SOLUTION 0
+
+/**
+ * Solution 0 - Binary Search
+ * Time Complexity:     O(log(n))
+ * Space Complexity:    O(1)
+ */
+#if RUN_SOLUTION==0
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        const int n = (int) nums.size();
+        if(n == 1) return nums[0];
+        int left = 0, right = n-1, mid;
+
+        while(left <= right) {
+            mid = (left + right) / 2;
+
+            // Look for point where value decreases rather than increases
+            if(mid && nums[mid] < nums[mid-1]) return nums[mid];
+
+            if(nums[mid] >= nums[left]) {
+                // Currently in left array
+                if(nums[left] < nums[right]) return nums[left];
+
+                // There is still a left and right array
+                left = mid+1;
+            } else {
+                // Currently in right array
+                right = mid-1;
+            }
+        }
+        return -1; // Should never reach here
+    }
+};
+#endif
+
+int main() {
+    Solution sol;
+    vector<int> nums;
+
+    nums = {3,4,5,1,2};
+    std::cout << sol.findMin(nums) << std::endl;
+
+    nums = {4,5,6,7,0,1,2};
+    std::cout << sol.findMin(nums) << std::endl;
+
+    nums = {11,13,15,17};
+    std::cout << sol.findMin(nums) << std::endl;
+
+    nums = {1};
+    std::cout << sol.findMin(nums) << std::endl;
+
+    nums = {1,2};
+    std::cout << sol.findMin(nums) << std::endl;
+
+    return 0;
+}
