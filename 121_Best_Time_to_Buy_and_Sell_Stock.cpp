9b5f9af89d219fa51773578e4320820d68032631
Omar Abouelleil <oabouelleil@live.com>
2022-09-15 21:10:33 +0100

    Add 121

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define RUN_SOLUTION 0
+
+/**
+ * Solution 0 - Min Buy
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+#if RUN_SOLUTION == 0
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int minBuy = INT_MAX;
+        int maxDifference = 0;
+
+        for (const auto &price: prices) {
+            minBuy = min(minBuy, price);
+            maxDifference = max(maxDifference, price - minBuy);
+        }
+
+        return maxDifference;
+    }
+};
+
+#endif
+
+int main() {
+    Solution sol;
+    vector<int> prices;
+
+    // 5
+    prices = {7, 1, 5, 3, 6, 4};
+    std::cout << sol.maxProfit(prices) << std::endl;
+
+    // 0
+    prices = {7, 6, 4, 3, 1};
+    std::cout << sol.maxProfit(prices) << std::endl;
+
+    return 0;
+}
